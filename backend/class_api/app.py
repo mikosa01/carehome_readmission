08938309fe01484316b3d7b65bdf629e5746ee95
mikosa01 <michaelmerry8@gmail.com>
2025-01-09 08:32:20 +0000

    my updated commit-17

--- backend/class_api/app.py
+++ backend/class_api/app.py
@@ -28,7 +28,7 @@ model_path = os.path.join(os.path.dirname(__file__), '../classification/model/mo
 with open (model_path, 'rb') as file:
     model = pickle.load(file)
 
-
+data_path = os.path.join(os.path.dirname(__file__), '../classification/data/processed/clean_data.csv')
 # model = mlflow.sklearn.load_model(artifact)
 
 @app.route('/')
@@ -70,11 +70,11 @@ def predict():
                                 "family_involvement", "hospital_acquired_infections", "discharge_timing", "chronic_conditions", 
                                 "previous_hospitalizations", "age", 'readmitted_within_30_days', 'admission_date'])
 
-    clean_data = pd.read_csv(f'../classification/data/processed/clean_data.csv')
+    clean_data = pd.read_csv(data_path)
     clean_data.reset_index(drop=True, inplace=True)
     new_val.reset_index(drop=True, inplace=True)
     update_data = pd.concat([clean_data,new_val], axis = 0, ignore_index=True)
-    update_data.to_csv(f'../classification/data/processed/clean_data.csv', index=False)
+    update_data.to_csv(data_path, index=False)
 
     readmission_pred = model.predict_proba(X_val)
     output = '{0:.{1}f}'.format(readmission_pred[0][1], 2)
