f21e395f551ea7eea01fbd98d3fd794eef499337
mikosa01 <michaelmerry8@gmail.com>
2025-01-19 17:32:41 +0000

    forntend commit

--- frontend/class_react/client/src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
-//Use this to create a proxy to the Express server port 5000 instead of specifying proxy in package.json
-module.exports = app => {
-    //Sends proxy request to http://localhost:5000/*
-    app.use('/server/*', createProxyMiddleware({ target:     `http://localhost:5000`, changeOrigin: true }))
-};
